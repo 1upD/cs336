3147d75e62e75576c04132f4fc4bd10105e39068
Derek Dik <drd26@chomsky.cs.calvin.edu>
2016-10-05 16:54:00 -0400

    I originally forgot to send the 404 error for bad requests. Updated to send 404 status whenever the user accesses a page or item that doesn't exist.

--- homework01/index.js
+++ homework01/index.js
@@ -14,7 +14,7 @@ function getPerson(id){
 return people.filter(
 	function(obj){
 		if(obj.id == id){
-			return obj		
+			return obj;		
 		}
 	})[0];
 }
@@ -23,33 +23,56 @@ return people.filter(
 // Some last names are not applicable
 function getName(id){
 	person = getPerson(id);
-	return person.firstname + " " + person.lastname
+	if(person === null || person === undefined){
+		return null;
+	}	
+	return person.firstname + " " + person.lastname;
 }
 
 // Functions to get years with organization
 function getYears(id){
 	person = getPerson(id);
-	return person.years
+	if(person === null || person === undefined){
+		return null;
+	}	
+	return person.years;
+}
+
+// Function to send a value as JSON. If value is null, send null instead
+function sendJSON(res, value){
+  if(value == null){
+    res.sendStatus(404);
+  } else {
+    res.json(value);
+  }
 }
 
 // Route /people returns all records
 app.get('/people', function (req, res) {
-  res.json(people);
+  sendJSON(res, people);
 });
 
 // Route /person/id returns one record by id
 app.get('/person/:id', function (req, res) {
-  res.json(getPerson(req.params.id));
+  var person = getPerson(req.params.id);
+  sendJSON(res, person);
 });
 
 // Route /person/id/name returns a first and last name by id
 app.get('/person/:id/name', function (req, res) {
-  res.json(getName(req.params.id));
+  var name = getName(req.params.id);
+  sendJSON(res, name);
 });
 
 // Route /person/id/years returns years within organization by id
 app.get('/person/:id/years', function (req, res) {
-  res.json(getYears(req.params.id));
+  var years = getYears(req.params.id);
+  sendJSON(res, years);
+});
+
+// Any other route should serve a 404 message.
+app.get('/*', function(req, res) {
+  res.sendStatus(404); 
 });
 
 // Start the server on port 3000
