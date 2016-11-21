41bcc4c52ed45455ebbb0865b272cf7cb4529899
Derek Dik <drd26@aiken.cs.calvin.edu>
2016-11-21 12:56:44 -0500

    Replaced password with environment variable

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient
 var dbConnection;
 
+var url = 'mongodb://cs336:'+process.env.PASSWORD+'@ds049624.mlab.com:49624/cs336';
 
-MongoClient.connect('mongodb://cs336:PASSWORD@ds049624.mlab.com:49624/cs336', function (err, db) {
+MongoClient.connect(url, function (err, db) {
   if (err) throw err
   dbConnection = db;
 });
